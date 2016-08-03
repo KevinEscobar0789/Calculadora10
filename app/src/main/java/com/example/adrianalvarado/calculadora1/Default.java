cce21a1107cc8c4083bd27521ec05ef49ca721cb
Adrian77990 <adbi_90@hotmail.com>
2016-08-03 17:17:55 -0600

    Calculadora1
    Practica2

---
+++ app/src/main/java/com/example/adrianalvarado/calculadora1/Default.java
@@ -0,0 +1,24 @@
+package com.example.adrianalvarado.calculadora1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Default extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default);
+    }
+
+    @Override
+     public boolean{
+
+    }
+}
